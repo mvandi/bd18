83dcca151ed5a0027a7235fb1a6de989e8061cca
Mattia Vandi <mattia.vandi@studio.unibo.it>
2019-03-15 10:48:52 +0100

    Fixed groupId

--- src/main/java/lab1/exercise4/Ex4InvertedIndex.java
+++ src/main/java/lab1/exercise4/Ex4InvertedIndex.java
@@ -23,9 +23,9 @@ public class Ex4InvertedIndex {
         private Text word = new Text();
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            LongWritable lineNumber = (LongWritable) key;
             StringTokenizer itr = new StringTokenizer(value.toString());
 
-            LongWritable lineNumber = (LongWritable) key;
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, lineNumber);
